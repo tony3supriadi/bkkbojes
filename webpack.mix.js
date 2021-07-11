26866be4861dc6f97e5fb27e45ea2274192abbbc
tony3supriadi <tony3supriadi@gmail.com>
2021-07-12 04:18:51 +0700

    bootsrap-install

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require("tailwindcss"),
-    ]);
+    .sass('resources/scss/app.scss', 'public/css');
