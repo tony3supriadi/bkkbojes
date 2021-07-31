6ea4ee6fb7392fa9fb2be6421148e3dadee8d6d8
sarwono <scqolbu@gmail.com>
2021-07-31 11:34:21 +0700

    Admin Backend

---
+++ database/migrations/2021_07_31_112021_create_bpg1_admin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBpg1AdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bpg1_admin', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama',64);
+            $table->string('username',64)->unique();
+            $table->string('password');
+            $table->string('email',128)->unique();
+            $table->enum('level',['Super Admin','Admin'])->nullable();           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bpg1_admin');
+    }
+}
