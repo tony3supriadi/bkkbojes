280258492af90347604c2cab6c5f2720c16e7cad
sarwono <scqolbu@gmail.com>
2021-08-02 23:24:48 +0700

    20210802_refisi_sidebar_admin

--- resources/views/admin/layouts/inc/sidebar.blade.php
+++ resources/views/admin/layouts/inc/sidebar.blade.php
@@ -15,66 +15,72 @@
         </a>
     </li>
 
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.manage-admin.index') }}">
-            <i class="fas fa-fw fa-user"></i>
-            <span>Manage Admin</span>
+    <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseMaster" 
+        aria-expanded="true" aria-controls="collapseMaster">
+            <i class="fas fa-fw fa-database"></i>
+            <span>Master Data</span>
         </a>
+        <div id="collapseMaster" class="collapse" aria-labelledby="headingMaster"
+        data-parent="#accordionSidebar" style="">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Master Data:</h6>
+                <a class="collapse-item" href="{{ route('admin.pengguna.index') }}">Pengguna</a>
+                <a class="collapse-item" href="{{ route('admin.perusahaan.index') }}">Mitra</a>
+            </div>
+        </div>
     </li>
 
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.lowongan.index') }}">
-            <i class="fas fa-fw fa-mail-bulk"></i>
-            <span>Lowongan</span>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTransaction" 
+        aria-expanded="false" aria-controls="collapseTransaction">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Transaction</span>
         </a>
+        <div id="collapseTransaction" class="collapse" aria-labelledby="headingTransaction"
+        data-parent="#accordionSidebar" style="">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Transaction:</h6>
+                <a class="collapse-item" href="{{ route('admin.lowongan.index') }}">Lowongan</a>
+                <a class="collapse-item" href="{{ route('admin.pengumuman.index') }}">Pengumuman</a>
+            </div>
+        </div>
     </li>
 
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.pengumuman.index') }}">
-            <i class="fas fa-fw fa-info-circle"></i>
-            <span>Pengumuman</span>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" 
+        aria-expanded="false" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Pages</span>
         </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages"
+        data-parent="#accordionSidebar" style="">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Pages:</h6>
+                <a class="collapse-item" href="#">Sering Ditanya (FAQ)</a>
+                <a class="collapse-item" href="#">Ketentuan Penggunaan</a>
+                <a class="collapse-item" href="#">Kebijakan Privasi</a>
+                <a class="collapse-item" href="#">Tentang Kami</a>
+                <a class="collapse-item" href="#">Testimonial</a>
+            </div>
+        </div>
     </li>
 
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.pengguna.index') }}">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Pengguna</span>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSetting" 
+        aria-expanded="false" aria-controls="collapseSetting">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Setting</span>
         </a>
-    </li>
-
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.bursakerja.index') }}">
-            <i class="fas fa-fw fa-briefcase"></i>
-            <span>Bursa Kerja</span>
-        </a>
-    </li>
-
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.perusahaan.index') }}">
-            <i class="fas fa-fw fa-handshake"></i>
-            <span>Perusahaan</span>
-        </a>
-    </li>
-
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.bantuan.index') }}">
-            <i class="fas fa-fw fa-question-circle"></i>
-            <span>Bantuan</span>
-        </a>
-    </li>
-
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.lainnya.index') }}">
-            <i class="fas fa-fw fa-layer-group"></i>
-            <span>Lainnya</span>
-        </a>
-    </li>
-
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('admin.pengaturan.index') }}">
-            <i class="fas fa-fw fa-cogs"></i>
-            <span>Pengaturan</span>
-        </a>
+        <div id="collapseSetting" class="collapse" aria-labelledby="headingSetting"
+        data-parent="#accordionSidebar" style="">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Setting:</h6>
+                <a class="collapse-item" href="{{ route('admin.manage-admin.index') }}">Manage Admin</a>
+                <a class="collapse-item" href="#">Alamat</a>
+                <a class="collapse-item" href="#">Hak Cipta</a>
+                <a class="collapse-item" href="#">Link</a>
+            </div>
+        </div>
     </li>
 </ul>
