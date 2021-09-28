ff3567cf1596079b30465366b7bf617d9137a5e6
tonisteb <toni@stebasia.com>
2021-09-29 05:52:23 +0700

    faq

--- resources/views/pages/faq/faq.blade.php
+++ resources/views/pages/faq/faq.blade.php
@@ -16,8 +16,20 @@
     <div class="container pt-4 pb-5">
         <div class="row">
             <div class="col-md-9">
-                <!-- Cari Lowongan -->
-                {{-- @include('pages.lowongan.partials.cari-lowongan') --}}
+                <div class="card card-body border-0 box-card p-md-4 p-3 mb-3">
+                    <div class="accordion accordion-flush">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <strong>Accordion Item #1</strong>
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-md-3">
                 <!-- Lowongan Kerja -->
